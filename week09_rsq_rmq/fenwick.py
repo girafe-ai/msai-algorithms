29410651ca0c81b360105afff6d9b8796693ce0b
i.solomatin <i.solomatin@samsung.com>
2021-12-21 16:48:13 +0300

    Finwick -> 1D case

--- week09_rsq_rmq/fenwick.py
+++ week09_rsq_rmq/fenwick.py
@@ -1,40 +1,27 @@
 class RSQFenwick:
-    def __init__(self, N, M):
+    def __init__(self, N):
         self.N = N
-        self.M = M
-        self.f = [[0] * self.M for i in range(self.N)]
+        self.f = [0] * self.N
 
-    def query(self, i, j):
+    def query(self, i):
         res = 0
-        print('===')
         while i >= 0:
-            while j >= 0:
-                print(i, j)
-                res += self.f[i][j]
-                j -= ~j & (j + 1)
+            res += self.f[i]
             i -= ~i & (i + 1)
-        print('===')
         return res
 
-    def update(self, i, j, delta):
+    def update(self, i, delta):
         while i < self.N:
-            while j < self.M:
-                self.f[i][j] += delta
-                j += ~j & (j + 1)
+            self.f[i] += delta
             i += ~i & (i + 1)
 
     def rsq(self, left, right):
         return self.query(right - 1) - self.query(left - 1)
 
-from random import randint
+
 if __name__ == '__main__':
-    #a = list(map(int, input().split()))
-    #N = int(input())
-    s = RSQFenwick(100, 100)
-    while True:
-        s.query(randint(0, 99), randint(0, 99))
-
-
+    N = int(input())
+    s = RSQFenwick(N)
     while True:
         a, b, c = input().split()
         if a == '+':
