a0aa32eb3c86a94f447b8ddd49e5dd18a9375ead
Ivan Solomatin <ivansolomat@yandex.ru>
2025-09-08 12:19:10 +0300

    Added W01 materials

---
+++ week01_sorting_algorithms/qsort_template.py
@@ -0,0 +1,35 @@
+import random
+
+
+def partition(x, l, r, pivot):
+    """
+    :param x: Source array (list)
+    :param l: left border of partitioning range (int)
+    :param r: right border (not included) of partitioning range (int)
+    :param pivot: pivot element to divide array (any item from x[l, r)).
+    :return: il, ir -- desired partition
+    This function should reorder elements of x within [l, r) region
+    in the way, these conditions are true:
+    x[l:il] < pivot
+    x[il:ir] == pivot
+    x[ir:r] > pivot
+    """
+    # Your code goes here
+    pass
+
+
+def qsort(x, l=0, r=None):
+    if r is None:
+        r = len(x)
+    if (r - l) > 1:
+        pivot = x[random.randint(l, r - 1)]
+        il, ir = partition(x, l, r, pivot)
+        qsort(x, l, il)
+        qsort(x, ir, r)
+
+
+if __name__ == '__main__':
+    N = int(input())
+    x = list(map(int, input().split()))
+    qsort(x)
+    print(' '.join(map(str, x)))
